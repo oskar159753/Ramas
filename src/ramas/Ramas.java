88e5d5ccfd5a365c4b395146bc9a644114a976fb
dam <dam@172.26.207.124>
2019-01-08 09:27:32 +0100

    Version 2.0

--- src/ramas/Ramas.java
+++ src/ramas/Ramas.java
@@ -15,7 +15,7 @@ public class Ramas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Version 1.0");
+        System.out.println("Version 2.0");
     }
     
 }
